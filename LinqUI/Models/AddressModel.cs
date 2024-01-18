f938a2f95ab258c7ebbce6537e031c9939ce24c3
yoomster <nperenboom@hotmail.com>
2024-01-18 17:32:00 +0100

    added contact id property to make use of one to many relationship

--- LinqUI/Models/AddressModel.cs
+++ LinqUI/Models/AddressModel.cs
@@ -9,6 +9,7 @@ namespace LinqUI.Models
     public class AddressModel
     {
         public int Id { get; set; }
+        public int ContactId { get; set; }
         public string Address { get; set; }
         public int HouseNr { get; set; }
         public string City { get; set; }
